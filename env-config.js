dffade8fb57bcb5ad822add3c0c5ef506b0e3bfa
Олексiй <alexey@MacBook-Air-Oleksij.local>
2021-06-09 01:18:38 +0300

    Updates

---
+++ env-config.js
@@ -0,0 +1,3 @@
+window._env_ = {
+  SOME_VARIABLE: "SOME-VALUE",
+}
---
+++ precache-manifest.95c12f91e841b16f60ad240b3e7e67d2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebcd4a9bfcdb535bc2aae3c5ebde6a3e",
+    "url": "/inko/kirill-task/assets/CloseMenu.svg"
+  },
+  {
+    "revision": "63ceb31ccd8d473199be3cfe40e5caa3",
+    "url": "/inko/kirill-task/assets/SupportTelegram.svg"
+  },
+  {
+    "revision": "95e7f044e956a04dad600453e81213aa",
+    "url": "/inko/kirill-task/assets/bg.svg"
+  },
+  {
+    "revision": "5f52fb6f8fabed9a4ab2450a5780d919",
+    "url": "/inko/kirill-task/assets/menu.svg"
+  },
+  {
+    "revision": "142eae9aa42d2a3cf7205d04a55de92f",
+    "url": "/inko/kirill-task/index.html"
+  },
+  {
+    "revision": "d4a405eb988e02a9dc81",
+    "url": "/inko/kirill-task/static/css/main.3d497511.chunk.css"
+  },
+  {
+    "revision": "3d567fbe9f8c4e0c1232",
+    "url": "/inko/kirill-task/static/js/2.6b93126b.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/inko/kirill-task/static/js/2.6b93126b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4a405eb988e02a9dc81",
+    "url": "/inko/kirill-task/static/js/main.22aba149.chunk.js"
+  },
+  {
+    "revision": "c62503f7c26ec8061541",
+    "url": "/inko/kirill-task/static/js/runtime-main.8d1a3597.js"
+  },
+  {
+    "revision": "ebcd4a9bfcdb535bc2aae3c5ebde6a3e",
+    "url": "/inko/kirill-task/static/media/CloseMenu.ebcd4a9b.svg"
+  },
+  {
+    "revision": "63ceb31ccd8d473199be3cfe40e5caa3",
+    "url": "/inko/kirill-task/static/media/SupportTelegram.63ceb31c.svg"
+  },
+  {
+    "revision": "5f52fb6f8fabed9a4ab2450a5780d919",
+    "url": "/inko/kirill-task/static/media/menu.5f52fb6f.svg"
+  }
+]);
